77d3c444ca345c2a007e738dcac98806c63849b9
dnl13 <schockomilch@googlemail.com>
2023-11-01 14:07:47 +0100

    fixes sdxl clip_l

--- efficiency_nodes.py
+++ efficiency_nodes.py
@@ -4110,7 +4110,7 @@ class TSC_Tiled_Upscaler:
     @classmethod
     def INPUT_TYPES(cls):
         # Split the list based on the keyword "tile"
-        cnet_filenames = [name for name in folder_paths.get_filename_list("controlnet") if "tile" in name]
+        cnet_filenames = [name for name in folder_paths.get_filename_list("controlnet")]
 
         return {"required": {"upscale_by": ("FLOAT", {"default": 1.25, "min": 0.01, "max": 8.0, "step": 0.05}),
                              "tile_size": ("INT", {"default": 512, "min": 256, "max": MAX_RESOLUTION, "step": 64}),
